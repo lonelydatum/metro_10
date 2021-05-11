a0060cb68cff5b9055bb6fe852b08466087ddb93
Gar <gar@lonelydatum.com>
2021-05-11 11:19:51 -0400

    d

--- dev/batter_300x250/_bundled/main.js
+++ dev/batter_300x250/_bundled/main.js
@@ -25,11 +25,21 @@ exports.size = size;
 exports.init = init;
 
 },{}],2:[function(require,module,exports){
-'use strict';
+"use strict";
 
 var _commonJsCommonJs = require('../../_common/js/common.js');
 
 var tl = (0, _commonJsCommonJs.init)();
+tl.to(".holder", .3, { x: -300 }, "+=3.2");
+tl.to(".holder", .3, { x: -600 }, "+=2");
+tl.to(".holder", .3, { x: -900 }, "+=2");
+tl.to(".holder", .3, { x: -1200 }, "+=2");
+tl.add("end", "+=.5");
+tl.from(".end_txt", .3, { opacity: 0 }, "end");
+tl.from(".end_copy", .3, { opacity: 0 }, "+=.3");
+tl.from([".end_cta", ".end_logo"], .3, { opacity: 0 }, "+=.5");
+
+// tl.gotoAndPlay("end")
 
 module.exports = {};
 
--- dev/batter_300x250/_js/main.js
+++ dev/batter_300x250/_js/main.js
@@ -4,8 +4,16 @@
 import {init} from '../../_common/js/common.js'
 
 const tl = init()
+tl.to(".holder", .3, {x:-300}, "+=3.2")
+tl.to(".holder", .3, {x:-600}, "+=2")
+tl.to(".holder", .3, {x:-900}, "+=2")
+tl.to(".holder", .3, {x:-1200}, "+=2")
+tl.add("end", "+=.5")
+tl.from(".end_txt", .3, {opacity:0}, "end")
+tl.from(".end_copy", .3, {opacity:0}, "+=.3")
+tl.from([".end_cta", ".end_logo"], .3, {opacity:0}, "+=.5")
 
-
+// tl.gotoAndPlay("end")
 
 module.exports = {};
 
